9dcd8fe85d7aaac6eceeb1bead1845c951c5b892
nikitanevskij <bike.rental@yandex.by>
2023-01-22 15:26:01 +0300

    feature fethParts

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+import { Layout } from 'antd';
+import { Result } from './components/Result/Result';
+import { Search } from './components/Search/Search';
+import axios from 'axios';
+
+const { Content } = Layout;
+
+const key = '';
+
+export const App = () => {
+  const [status, setStatus] = React.useState('success');
+  const [articles, setArticles] = React.useState([]);
+  const [fetchResult, setFetchResult] = React.useState([]);
+
+  React.useEffect(() => {
+    if (!articles.length || !articles[0].length) return;
+    async function fethParts() {
+      try {
+        setStatus('loading');
+        setArticles([]);
+        const { data } = await axios.post(
+          'https://germsp.ru/test-search-products',
+          { searchArticles: articles },
+          {
+            headers: {
+              Authorization: `Bearer ${key}`,
+            },
+          },
+        );
+        setStatus('success');
+        setFetchResult(data);
+      } catch (error) {
+        setStatus('error');
+      }
+    }
+    fethParts();
+  }, [articles]);
+
+  return (
+    <Layout className="layout">
+      <Content className="content">
+        <Search setArticles={setArticles} status={status} />
+        <Result data={fetchResult} />
+      </Content>
+    </Layout>
+  );
+};
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import './search.css';
-import { Input, Button } from 'antd';
+import { Input, Button, Alert } from 'antd';
 
 const { TextArea } = Input;
 
-export const Search = () => {
+export const Search = ({ setArticles, status }) => {
   const [value, setValue] = React.useState('');
 
   const onChangeValue = (e) => {
     setValue(e.target.value);
   };
 
-  const createArrayArticles = () => {
-    const array = value.split('\n');
+  const createArticles = () => {
+    const arcticles = value.split('\n');
+    setArticles(arcticles);
   };
 
   return (
     <div className="search-block">
       <h3 className="search-title">Товары</h3>
+      {status === 'error' && (
+        <Alert
+          message="Внутренняя ошибка сервера. Попробуйте снова."
+          type="warning"
+          showIcon
+          style={{ marginBottom: '20px', width: '100%' }}
+        />
+      )}
       <TextArea
         value={value}
         onChange={onChangeValue}
@@ -25,7 +34,7 @@ export const Search = () => {
         rows={4}
         placeholder="Для поиска введите один или несколько артукулов в столбик"
       />
-      <Button type="primary" className="search-button" onClick={createArrayArticles}>
+      <Button type="primary" className="search-button" onClick={createArticles}>
         Поиск
       </Button>
     </div>
