553756f246946cf42f06636a5c8084c430e71ada
nikitanevskij <bike.rental@yandex.by>
2023-01-22 13:58:43 +0300

    add feature in Search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import './search.css';
+import { Input, Button } from 'antd';
+
+const { TextArea } = Input;
 
 export const Search = () => {
-  return <div>Search</div>;
+  const [value, setValue] = React.useState('');
+
+  const onChangeValue = (e) => {
+    setValue(e.target.value);
+  };
+
+  const createArrayArticles = () => {
+    const array = value.split('\n');
+  };
+
+  return (
+    <div className="search-block">
+      <h3 className="search-title">Товары</h3>
+      <TextArea
+        value={value}
+        onChange={onChangeValue}
+        className="search-input"
+        rows={4}
+        placeholder="Для поиска введите один или несколько артукулов в столбик"
+      />
+      <Button type="primary" className="search-button" onClick={createArrayArticles}>
+        Поиск
+      </Button>
+    </div>
+  );
 };
